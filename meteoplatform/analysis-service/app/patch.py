7e9284d6bad03fad91d647a7c3314a9fbc8f7c27
scalisesufa-tech <scalisesufa@gmail.com>
2026-03-09 21:15:07 +0100

    Delete meteoplatform directory

--- meteoplatform/analysis-service/app/patch.py
+++
@@ -1,8 +0,0 @@
-from fastapi.exceptions import RequestValidationError
-from fastapi.responses import JSONResponse
-
-@app.exception_handler(RequestValidationError)
-async def validation_exception_handler(request, exc):
-    import logging
-    logging.error(f"Validation Error: {exc.errors()} Body: {exc.body}")
-    return JSONResponse(status_code=422, content={"detail": exc.errors()})
